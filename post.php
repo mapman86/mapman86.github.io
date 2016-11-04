50ede6df037971eb60b5aa9ed5fde11019c7e4a3
Matt <matthewmccullough86@gmail.com>
2016-11-04 14:52:32 -0600

    Update post.php

--- post.php
+++ post.php
@@ -1,12 +1,11 @@
 <?php $name = $_POST['name'];
 $email = $_POST['email'];
-$subject = $_POST['subject'];
 $message = $_POST['message'];
-$recipient = "cartedesignmaps@gmail.com";
+$recipient = "matthewmccullough86@gmail.com";
 $mailheader = "From: $email \r\n";
 $formcontent="From: $name \n Message: $message";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
 echo "Your message has been sent. Thank You!";
 ?>
 
-<p><a href="http://www.cartedesign.com">Click here</a> to return to the previous page.</p>
+<p><a href="https://www.mapman86.github.io">Click here</a> to return to the previous page.</p>
